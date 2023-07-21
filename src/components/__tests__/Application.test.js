d6f93a1031e177ab0232ec358009971e2d58c5d2
wfhopkins <will.hop@gmail.com>
2023-07-20 20:19:44 -0700

    a bunch of tests updated to use mocks...i think

---
+++ src/components/__tests__/Application.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {fireEvent, render, waitForElement } from "@testing-library/react";
+import Application from "components/Application";
+
+
+describe("Application", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+    
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
+  });
+})
